f42947b5977f2e26284350c79b763aa13132bfe8
Mopano <p.r.7.norouzi@gmail.com>
2022-12-06 01:00:19 -0800

    Added styles and first dynamics

--- src/components/Eitems.js
+++ src/components/Eitems.js
@@ -1,5 +1,19 @@
+import "./Eitems.css";
+
 function EItems() {
-    return <h2>hi i am here !</h2>;
+    const eTime = "2022/12/06";
+    const eItem = "Car Price";
+    const eItemPrice = 300;
+
+    return (
+        <div className="expense-item">
+            <div>{eTime}</div>
+            <div className="expense-item__description">
+                <h2>{eItem}</h2>
+                <div className="expense-item__price">${eItemPrice}</div>
+            </div>
+        </div>
+    );
 }
 
 export default EItems;
