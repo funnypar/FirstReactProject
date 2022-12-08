912982154e99f3d2fd88962bd0931d26d7c09d47
Mopano <p.r.7.norouzi@gmail.com>
2022-12-08 10:05:26 -0800

    Added first eventHandler

--- src/components/Eitems/Eitems.js
+++ src/components/Eitems/Eitems.js
@@ -3,12 +3,15 @@ import EitemsDate from "./EitemsDate";
 import "./Eitems.css";
 
 const EItems = (props) => {
+    const hiHandler = () => console.log("hi there !!!");
+
     return (
         <Card className="expense-item">
             <EitemsDate dateItem={props.dateItem} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.price}</div>
+                <button onClick={hiHandler}>click here!</button>
             </div>
         </Card>
     );
