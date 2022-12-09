df8e5874743ad4d22f6607e608c6b2ebbfdb2e48
Mopano <p.r.7.norouzi@gmail.com>
2022-12-09 11:34:22 -0800

    Added First work with state

--- src/components/Eitems/Eitems.js
+++ src/components/Eitems/Eitems.js
@@ -1,16 +1,21 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import EitemsDate from "./EitemsDate";
 import "./Eitems.css";
 
 const EItems = (props) => {
-    const hiHandler = () => console.log("hi there !!!");
+    const [price, setPrice] = useState(props.price);
+
+    const hiHandler = () => {
+        setPrice("updated !!!");
+    };
 
     return (
         <Card className="expense-item">
             <EitemsDate dateItem={props.dateItem} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.price}</div>
+                <div className="expense-item__price">${price}</div>
                 <button onClick={hiHandler}>click here!</button>
             </div>
         </Card>
