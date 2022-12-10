962307fb107770d59d5f8ca14203fa9a5a1caf5a
Mopano <p.r.7.norouzi@gmail.com>
2022-12-10 06:43:12 -0800

    Added work with onChange

--- src/components/newEitem/newEitemForm.js
+++ src/components/newEitem/newEitemForm.js
@@ -2,18 +2,22 @@ import React from "react";
 import "./newEitemForm.css";
 
 const NewEitemForm = () => {
+    const cityChangeHandler = (e) => {
+        console.log(e.target.value);
+    };
+
     return (
         <form className="new-expense__controls">
             <div className="new-expense__controls">
-                <label for="city">City : </label>
-                <input type="text" />
+                <label htmlFor="city">City : </label>
+                <input type="text" onChange={cityChangeHandler} />
             </div>
             <div className="new-expense__controls">
-                <label for="price">Price : </label>
+                <label htmlFor="price">Price : </label>
                 <input type="number" min="0.01" step="0.01" />
             </div>
             <div className="new-expense__controls">
-                <label for="date">Date : </label>
+                <label htmlFor="date">Date : </label>
                 <input type="date" min="2018-01-01" max="2025-12-31" />
             </div>
             <div className="new-expense__actions">
