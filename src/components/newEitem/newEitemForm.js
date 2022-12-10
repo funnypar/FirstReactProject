9750058e77842e2e452cb238f6a0e598b482508f
Mopano <p.r.7.norouzi@gmail.com>
2022-12-10 07:23:54 -0800

    Added listenToDatas and save them with state

--- src/components/newEitem/newEitemForm.js
+++ src/components/newEitem/newEitemForm.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./newEitemForm.css";
 
 const NewEitemForm = () => {
+    const [enteredCity, setenteredCity] = useState("");
+    const [enteredPrice, setenteredPrice] = useState("");
+    const [enteredDate, setenteredDate] = useState("");
+
     const cityChangeHandler = (e) => {
-        console.log(e.target.value);
+        setenteredCity(e.target.value);
+        console.log(enteredCity);
+    };
+    const priceChangeHandler = (e) => {
+        setenteredPrice(e.target.value);
+        console.log(enteredPrice);
+    };
+    const dateChangeHandler = (e) => {
+        setenteredDate(e.target.vlaue);
+        console.log(enteredDate);
     };
 
     return (
@@ -14,11 +27,21 @@ const NewEitemForm = () => {
             </div>
             <div className="new-expense__controls">
                 <label htmlFor="price">Price : </label>
-                <input type="number" min="0.01" step="0.01" />
+                <input
+                    type="number"
+                    min="0.01"
+                    step="0.01"
+                    onChange={priceChangeHandler}
+                />
             </div>
             <div className="new-expense__controls">
                 <label htmlFor="date">Date : </label>
-                <input type="date" min="2018-01-01" max="2025-12-31" />
+                <input
+                    type="date"
+                    min="2018-01-01"
+                    max="2025-12-31"
+                    onChange={dateChangeHandler}
+                />
             </div>
             <div className="new-expense__actions">
                 <button type="submit">submit</button>
